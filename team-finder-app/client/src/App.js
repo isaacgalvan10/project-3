348e1b6507e529f745e01bb549fae707379404e2
abi <abimael.monarrez58@gmail.com>
2022-05-03 16:59:08 -0600

    create routes for phase 1

--- team-finder-app/client/src/App.js
+++ team-finder-app/client/src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Homepage from './pages/results/Homepage'
+import SearchResults from './pages/results/SearchResults';
+import Project from './pages/projects/Project';
+import MyProject from './pages/projects/MyProject';
+import Profile from './pages/profiles/Profile';
+import MyProfile from './pages/profiles/MyProfile';
+import Footer from './components/Footer';
+
+const httpLink = createHttpLink({
+  uri: 'graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Header />
+          <Routes>
+            <Route
+              path='/'
+              element={<Homepage />}
+            />
+            <Route
+              path='/searchResults'
+              element={<SearchResults />}
+            />
+            <Route
+              path='/project'
+              element={<Project />}
+            />
+            <Route
+              path='/myProject'
+              element={<MyProject />}
+            />
+            <Route
+              path='/profile'
+              element={<Profile />}
+            />
+            <Route
+              path='/myProfile'
+              element={<MyProfile />}
+            />
+          </Routes>
+          <Footer />
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ team-finder-app/client/src/pages/projects/MyProject.js
@@ -0,0 +1,7 @@
+const MyProject = () => {
+    return (
+        <h1>My Project</h1>
+    )
+}
+
+export default MyProject;
