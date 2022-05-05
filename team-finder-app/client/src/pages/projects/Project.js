12e38fa1d5b8cf24d31eb9c62fda36d992e88a81
abi <abimael.monarrez58@gmail.com>
2022-05-05 11:41:24 -0600

    create mockup of functionality in Project page from request side

--- team-finder-app/client/src/pages/projects/Project.js
+++ team-finder-app/client/src/pages/projects/Project.js
@@ -1,17 +1,33 @@
-import { Container, Row, Col, Button, Image, ListGroup, Toast, ToastContainer } from 'react-bootstrap';
+import { Container, Row, Col, Button, Image, ListGroup } from 'react-bootstrap';
 import { useState } from 'react';
 import '../../components/styles/project.css';
 import swal from "sweetalert";
-
+import Notification from '../../components/Notification';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Project = () => {
     const [request, setRequest] = useState('Ask to Join!');
     const [userPicture, setPicture] = useState('./no-profile-picture.jpeg');
     const [show, setShow] = useState(false);
+    const [text, setText] = useState('');
+    const [teamFull, setTeamFull] = useState(false);
+    const [joined, setJoined] = useState(false);
 
-    const sendRequest = async (status) => {
-        console.log(status);
-        if (status === 'Ask to Join!') {
+    const teamSize = 4;
+    const [teamMembers, setMembers] = useState(['member1', 'member2', 'member3']);
+
+    const spotsLeft = () => {
+        const spots = teamSize - teamMembers.length;
+        if (spots === 0) {
+            setTeamFull(true);
+        } 
+        
+        return spots;
+    }
+
+    const sendRequest = async (btnText) => {
+        if (btnText === 'Ask to Join!') {
             const confirm = await swal({
                 title: "Are you sure you want to join this team?",
                 buttons: ["No", "Yes"],
@@ -25,12 +41,27 @@ const Project = () => {
 
                 const responseInterval = setInterval(() => {
                     setShow(true);
+                    setText('lernantino has accepted your request, you are part of the team!');
                     setRequest('Dropout');
-                    setPicture('./pamela.jpeg')
+                    setPicture('./pamela.jpeg');
+                    setMembers([...teamMembers, 'member4']);
+                    setJoined(true);
                     clearInterval(responseInterval);
-                }, 5000)
+                }, 10000)
+
+
+                const kickedOut = setInterval(() => {
+                    setShow(true);
+                    setText('lernantino has kicked you out of their team')
+                    setRequest('Ask to Join!');
+                    setPicture('./no-profile-picture.jpeg');
+                    setMembers(['member1', 'member2', 'member3']);
+                    setJoined(false);
+                    setTeamFull(false);
+                    clearInterval(kickedOut);
+                }, 25000)
             }
-        } else if (status === 'Pending...') {
+        } else if (btnText === 'Pending...') {
             swal({
                 text: "You've already sent a request to join this team.",
             });
@@ -45,21 +76,18 @@ const Project = () => {
                     text: "You have dropout of lernantino's team",
                 });
                 setRequest('Ask to Join!');
-                setPicture('./no-profile-picture.jpeg')
+                setPicture('./no-profile-picture.jpeg');
+                setMembers(['member1', 'member2', 'member3']);
+                setJoined(false);
+                setTeamFull(false);
             }
-
         }
 
     }
 
     return (
         <>
-            <ToastContainer position="top-end">
-                <Toast onClose={() => setShow(false)} show={show} delay={7000} autohide>
-                    <Toast.Body>lernantino has accepted your request, you are part of the team!</Toast.Body>
-                </Toast>
-            </ToastContainer>
-
+            <Notification show={show} setShow={setShow} text={text} />
             <Container fluid className='d-flex flex-column align-items-center'>
                 <h1 className='mb-3'>Job Tracker App</h1>
                 <Row className='align-items-center mb-3'>
@@ -70,6 +98,11 @@ const Project = () => {
                         <p>May 5, 2021</p>
                     </Col>
                     <Button variant="success" onClick={(e) => sendRequest(e.target.textContent)}>{request}</Button>
+                    {joined ? (
+                        <a href='https://github.com/isaacgalvan10/project-3' target='_blank' rel='noreferrer' className='text-reset' ><FontAwesomeIcon icon={faGithub} className="gh-icon" /></a>
+                    ) : (
+                        null
+                    )}
                 </Row>
                 <Image src="./project.png" alt="project" className='project-img mb-3'></Image>
                 <ListGroup horizontal className='mb-3'>
@@ -89,7 +122,12 @@ const Project = () => {
                     We have two spots left! Preferably good with CSS.
                 </p>
                 <h3>Team Members</h3>
-                <p>2 Spots left!</p>
+                {!teamFull
+                    ? (
+                        <p>{spotsLeft()} Spots left!</p>
+                    ) : (
+                        null
+                    )}
                 <Row>
                     <Col>
                         <Image src="./lernantino.jpeg" alt="user" roundedCircle className='profile-img'></Image>
@@ -102,9 +140,6 @@ const Project = () => {
                     </Col>
                     <Col>
                         <Image src={userPicture} alt="empty" roundedCircle className='profile-img'></Image>
-                    </Col>
-                    <Col>
-                        <Image src="./no-profile-picture.jpeg" alt="empty" roundedCircle className='profile-img'></Image>
                     </Col>
                 </Row>
             </Container>
