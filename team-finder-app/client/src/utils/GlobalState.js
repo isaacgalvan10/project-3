78a6cb8037ac1f9c8bbfec2517fb7b19262fa1cb
Isaac Galvan <isaacgdev@gmail.com>
2022-05-05 21:29:23 -0500

    mapping projects

--- team-finder-app/client/src/utils/GlobalState.js
+++ team-finder-app/client/src/utils/GlobalState.js
@@ -1,16 +1,15 @@
-import React, { createContext, useContext } from "react";
-import { useGlobalReducer } from './reducers'
+import React, { createContext, useContext } from 'react';
+import { useGlobalReducer } from './reducers';
 
 const GlobalContext = createContext();
 const { Provider } = GlobalContext;
 
 const GlobalProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useGlobalReducer({
-
     notif: {
       show: false,
       route: '/',
-      text: ''
+      text: '',
     },
 
     projects: [
@@ -27,32 +26,60 @@ const GlobalProvider = ({ value = [], ...props }) => {
           {
             id: 1,
             picture: './lernantino.jpeg',
-            username: ''
+            username: '',
           },
           {
             id: 2,
             picture: './lernantino.jpeg',
-            username: ''
+            username: '',
           },
           {
             id: 3,
             picture: './lernantino.jpeg',
-            username: ''
+            username: '',
           },
           {
             id: 4,
             picture: './no-profile-picture.jpeg',
-            username: ''
+            username: '',
           },
-        ]
+        ],
+      },
+      {
+        title: 'Notes taker App',
+        date: 'May 5, 2022',
+        poster: 'qwerty',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        edited: true,
+        description: `This is an app that helps the user keep track of their job applications and reminds them what jobs they have applied to and update the status on their job applications and will remind them to follow up after a interview. I’m looking for a team of 5 with basic HTML, CSS, and JAVASCRIPT knowledge. \n Edit \n We have two spots left! Preferably good with CSS.`,
+        profile: './lernantino.jpeg',
+        projectImg: './project.png',
+      },
+      {
+        title: 'Ecommerce shop',
+        date: 'May 5, 2022',
+        poster: 'qwerty 2',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        edited: true,
+        description: `This is an app that helps the user keep track of their job applications and reminds them what jobs they have applied to and update the status on their job applications and will remind them to follow up after a interview. I’m looking for a team of 5 with basic HTML, CSS, and JAVASCRIPT knowledge. \n Edit \n We have two spots left! Preferably good with CSS.`,
+        profile: './lernantino.jpeg',
+        projectImg: './project.png',
+      },
+      {
+        title: 'Random Project',
+        date: 'May 5, 2022',
+        poster: 'qwerty 2',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        edited: true,
+        description: `This is an app that helps the user keep track of their job applications and reminds them what jobs they have applied to and update the status on their job applications and will remind them to follow up after a interview. I’m looking for a team of 5 with basic HTML, CSS, and JAVASCRIPT knowledge. \n Edit \n We have two spots left! Preferably good with CSS.`,
+        profile: './lernantino.jpeg',
+        projectImg: './project.png',
       },
     ],
 
     me: {
       status: 'out',
-    }
-
-
+    },
   });
 
   return <Provider value={[state, dispatch]} {...props} />;
