49859c3a70b2cf4c2215aba305f21fd1497f78e1
abi <abimael.monarrez58@gmail.com>
2022-05-06 17:41:44 -0600

    handle logic to simulate requests to join without setIntervals

--- team-finder-app/client/src/pages/projects/Project.js
+++ team-finder-app/client/src/pages/projects/Project.js
@@ -1,27 +1,57 @@
-import { Container, Row, Col, Button, Image, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Button, Image, ListGroup, Dropdown } from 'react-bootstrap';
 import { useState } from 'react';
 import '../../components/styles/project.css';
 import swal from "sweetalert";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { useGlobalContext } from '../../utils/GlobalState';
-import { SHOW_NOTIF, ADD_MEMBER, STATUS } from '../../utils/actions';
+import { SHOW_NOTIF, REMOVE_MEMBER, STATUS, SHOW_MODAL_NOTIF } from '../../utils/actions';
+import { Link } from 'react-router-dom';
 
 const Project = () => {
-    const [teamFull, setTeamFull] = useState(false);
     const [state, dispatch] = useGlobalContext();
 
-    const teamSize = 4;
-    const [teamMembers, setMembers] = useState(['member1', 'member2', 'member3']);
     const [isPoster, setPoster] = useState(false);
+    const emptySpots = state.projects[0].spotsLeft();
+    const myId = state.me.id;
 
     const switchUser = () => {
         if (!isPoster) {
             setPoster(true);
+
         } else {
             setPoster(false);
         }
     };
+
+    const removeMember = async (memberId, username) => {
+        const confirm = await swal({
+            title: `Are you sure you want to remove ${username} from your team?`,
+            buttons: ["No", "Yes"],
+        });
+
+        if (confirm) {
+            dispatch({
+                type: REMOVE_MEMBER,
+                id: memberId
+            });
+
+            if (memberId === myId) {
+                dispatch({
+                    type: STATUS,
+                    status: 'out'
+                });
+                dispatch({
+                    type: SHOW_NOTIF,
+                    payload: {
+                        text: 'lernantino has kicked you out of their team :(',
+                        route: '/project'
+                    }
+                });
+            }
+        }
+
+    }
 
     const setBtnText = () => {
         if (state.me.status === 'out') {
@@ -31,14 +61,6 @@ const Project = () => {
         } else {
             return 'Dropout'
         }
-    }
-    const spotsLeft = () => {
-        const spots = teamSize - teamMembers.length;
-        if (spots === 0) {
-            setTeamFull(true);
-        }
-
-        return spots;
     }
 
     const sendRequest = async (btnText) => {
@@ -53,57 +75,16 @@ const Project = () => {
                     text: "You have sent lernantino a request to join their team",
                 });
                 dispatch({
+                    type: SHOW_MODAL_NOTIF,
+                    payload: {
+                        text: `${state.me.username} has sent a request to join your team!`,
+                        route: '/project'
+                    }
+                });
+                dispatch({
                     type: STATUS,
                     status: 'pending'
                 });
-
-                const responseInterval = setInterval(() => {
-                    dispatch({
-                        type: SHOW_NOTIF,
-                        payload: {
-                            text: 'lernantino has accepted your request, you are part of the team!',
-                            route: '/project'
-                        }
-                    });
-                    dispatch({
-                        type: ADD_MEMBER,
-                        payload: {
-                            id: 4,
-                            picture: './pamela.jpeg'
-                        }
-                    });
-                    setMembers([...teamMembers, 'member4']);
-                    dispatch({
-                        type: STATUS,
-                        status: 'joined'
-                    });
-                    clearInterval(responseInterval);
-                }, 10000)
-
-
-                const kickedOut = setInterval(() => {
-                    dispatch({
-                        type: SHOW_NOTIF,
-                        payload: {
-                            text: 'lernantino has kicked you out of the team',
-                            route: '/project'
-                        }
-                    });
-                    dispatch({
-                        type: ADD_MEMBER,
-                        payload: {
-                            id: 4,
-                            picture: './no-profile-picture.jpeg'
-                        }
-                    });
-                    setMembers(['member1', 'member2', 'member3']);
-                    dispatch({
-                        type: STATUS,
-                        status: 'out'
-                    });
-                    setTeamFull(false);
-                    clearInterval(kickedOut);
-                }, 25000)
             }
         } else if (btnText === 'Pending...') {
             swal({
@@ -120,18 +101,13 @@ const Project = () => {
                     text: "You have dropout of lernantino's team",
                 });
                 dispatch({
-                    type: ADD_MEMBER,
-                    payload: {
-                        id: 4,
-                        picture: './no-profile-picture.jpeg'
-                    }
+                    type: REMOVE_MEMBER,
+                    id: myId
                 });
-                setMembers(['member1', 'member2', 'member3']);
                 dispatch({
                     type: STATUS,
                     status: 'out'
                 });
-                setTeamFull(false);
             }
         }
 
@@ -142,15 +118,35 @@ const Project = () => {
             <Button variant="success" onClick={() => switchUser()}>Switch</Button>
             <Container fluid className='d-flex flex-column align-items-center'>
                 <h1 className='mb-3'>{state.projects[0].title}</h1>
+                {!isPoster ? (
+                    <p>USER SIDE</p>
+                ) : (
+                    <p>POSTER SIDE</p>
+                )}
                 <Row className='align-items-center mb-3'>
                     <Image src={state.projects[0].profile} alt="user" roundedCircle className='profile-img'></Image>
                     <Col>
                         <p>{state.projects[0].poster}</p>
                         <p>{state.projects[0].date}</p>
                     </Col>
-                    <Button variant="success" onClick={(e) => sendRequest(e.target.textContent)}>{setBtnText()}</Button>
-                    {state.me.status === 'joined' ? (
-                        <a href='https://github.com/isaacgalvan10/project-3' target='_blank' rel='noreferrer' className='text-reset' ><FontAwesomeIcon icon={faGithub} className="gh-icon" /></a>
+                    {!isPoster ? (
+                        <Button variant="success" onClick={(e) => sendRequest(e.target.textContent)}>{setBtnText()}</Button>
+                    ) : (
+                        <>
+                            <Row className='edit-close-delete'>
+                                <p className='mr-1'>Edit |</p>
+                                <p className='mr-1'>Close |</p>
+                                <p className='mr-1'>Delete</p>
+                            </Row>
+                            <a href='https://github.com/isaacgalvan10/project-3' target='_blank' rel='noreferrer' className='text-reset' ><FontAwesomeIcon icon={faGithub} className="gh-icon" /></a>
+                        </>
+                    )}
+                    {!isPoster ? (
+                        state.me.status === 'joined' ? (
+                            <a href='https://github.com/isaacgalvan10/project-3' target='_blank' rel='noreferrer' className='text-reset' ><FontAwesomeIcon icon={faGithub} className="gh-icon" /></a>
+                        ) : (
+                            null
+                        )
                     ) : (
                         null
                     )}
@@ -158,7 +154,7 @@ const Project = () => {
                 <Image src={state.projects[0].projectImg} alt="project" className='project-img mb-3'></Image>
                 <ListGroup horizontal className='mb-3'>
                     {state.projects[0].technologies.map((tech) => (
-                        <ListGroup.Item>{tech}</ListGroup.Item>
+                        <ListGroup.Item key={tech.id}>{tech.techName}</ListGroup.Item>
                     ))}
                 </ListGroup>
                 <p>
@@ -173,17 +169,35 @@ const Project = () => {
                     {state.projects[0].description}
                 </p>
                 <h3>Team Members</h3>
-                {!teamFull
+                {emptySpots.length !== 0
                     ? (
-                        <p>{spotsLeft()} Spots left!</p>
+                        <p>{emptySpots.length} Spots left!</p>
                     ) : (
                         null
                     )}
                 <Row>
                     {state.projects[0].members.map((member) => (
-                        <Image src={member.picture} alt="user" roundedCircle className='profile-img'></Image>
+                        <Col key={member.id}>
+                            <Dropdown>
+                                <Dropdown.Toggle className='dropdown-custom'>
+                                    <Image src={member.picture} alt="user" roundedCircle className='profile-img'></Image>
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item as={Link} to='/profile'>{member.username}</Dropdown.Item>
+                                    {!isPoster ? (
+                                        null
+                                    ) : (
+                                        <Dropdown.Item onClick={() => removeMember(member.id, member.username)}>Remove from team</Dropdown.Item>
+                                    )}
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
                     )
                     )}
+                    {emptySpots.map((emptySpot) => (
+                        <Image src={emptySpot} alt="user" roundedCircle className='profile-img'></Image>
+                    ))}
                 </Row>
             </Container>
         </>
