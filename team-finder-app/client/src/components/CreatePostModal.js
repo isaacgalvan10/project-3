86cbd7de7174720151085cc1c7717c8815e4c206
abi <abimael.monarrez58@gmail.com>
2022-05-07 10:12:08 -0600

    handle logic to create a post (locally)

--- team-finder-app/client/src/components/CreatePostModal.js
+++ team-finder-app/client/src/components/CreatePostModal.js
@@ -1,21 +1,68 @@
 import { Modal, Button, Form, FloatingLabel } from 'react-bootstrap';
 import { useGlobalContext } from '../utils/GlobalState';
-import { HIDE_MODAL } from '../utils/actions';
+import { HIDE_MODAL, ADD_PROJECT } from '../utils/actions';
 import { useState } from 'react';
 
 const CreatePostModal = () => {
     const [state, dispatch] = useGlobalContext();
     const [validated, setValidated] = useState(false);
 
-    const handleSubmit = (event) => {
+    const [postFormData, setPostFormData] = useState({ 
+        title: '',
+        tags: '', 
+        description: '',
+        team: '',
+        projectImg: ''
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setPostFormData({ ...postFormData, [name]: value });
+      };
+
+    const handleSubmit = async (event) => {
         const form = event.currentTarget;
+        event.preventDefault();
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
         }
 
+        const tagsArr = postFormData.tags.split(', ');
+
+        const tagsArrObj = [];
+
+        for (let tag in tagsArr) {
+            tagsArrObj.push({
+                id: +tag + 1,
+                tagName: tagsArr[tag]
+            });
+        };
         
-    
+        const post = {
+            ...postFormData,
+            tags: tagsArrObj
+        }
+
+        console.log(post);
+
+        const addProject = await dispatch({
+            type: ADD_PROJECT,
+            newProject: post
+        });
+
+        setPostFormData({ 
+            title: '',
+            tags: '', 
+            description: '',
+            team: '',
+            projectImg: ''
+        });
+
+        dispatch({
+            type: HIDE_MODAL,
+        });
+
         setValidated(true);
       };
     
@@ -37,6 +84,8 @@ const CreatePostModal = () => {
                             type='text'
                             placeholder='Your title'
                             name='title'
+                            onChange={handleInputChange}
+                            value={postFormData.title}
                             required
                         />
                         <Form.Control.Feedback type='invalid'>Title is required!</Form.Control.Feedback>
@@ -48,6 +97,8 @@ const CreatePostModal = () => {
                             type='text'
                             placeholder='HTML, CSS, JavaScript'
                             name='tags'
+                            onChange={handleInputChange}
+                            value={postFormData.tags}
                             required
                         />
                         <Form.Control.Feedback type='invalid'>At least one tag is required!</Form.Control.Feedback>
@@ -58,7 +109,10 @@ const CreatePostModal = () => {
                         <FloatingLabel controlId="floatingTextarea2">
                             <Form.Control
                                 as="textarea"
+                                name='description'
                                 style={{ height: '100px' }}
+                                onChange={handleInputChange}
+                                value={postFormData.description}
                                 required
                             />
                             <Form.Control.Feedback type='invalid'>A description is required!</Form.Control.Feedback>
@@ -72,9 +126,11 @@ const CreatePostModal = () => {
                             min="1"
                             placeholder='Besides you, how many members do you want in your team?'
                             name='team'
+                            onChange={handleInputChange}
+                            value={postFormData.team}
                             required
                         />
-                        <Form.Control.Feedback type='invalid'>Team Size is required!</Form.Control.Feedback>
+                        <Form.Control.Feedback type='invalid'>A number is required!</Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group>
@@ -82,7 +138,9 @@ const CreatePostModal = () => {
                         <Form.Control
                             type='text'
                             placeholder='Image link'
-                            name='image'
+                            name='projectImg'
+                            onChange={handleInputChange}
+                            value={postFormData.projectImg}
                         />
                     </Form.Group>
                     
