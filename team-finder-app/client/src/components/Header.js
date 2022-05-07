048822aaa8e2d89a285f8cb2cb23dd85b5957d55
Isaac Galvan <isaacgdev@gmail.com>
2022-05-06 20:25:53 -0500

    Updating styles on home page and header

--- team-finder-app/client/src/components/Header.js
+++ team-finder-app/client/src/components/Header.js
@@ -4,16 +4,15 @@ import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
-import { Offcanvas, Image } from 'react-bootstrap';
+import { Offcanvas, Image, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './styles/header.css';
-import { useState } from 'react';
 
 const Header = () => {
   return (
     <>
       {['md'].map((expand) => (
-        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+        <Navbar key={expand} expand={expand} className="mb-3">
           <Container>
             <Navbar.Brand as={Link} to="/">
               Team Finder
@@ -24,27 +23,44 @@ const Header = () => {
               aria-labelledby={`offcanvasNavbarLabel-expand-md`}
               placement="end"
             >
+              <Offcanvas.Header
+                closeButton
+                className="justify-content-end"
+              ></Offcanvas.Header>
               <Offcanvas.Body className="justify-content-end">
                 <Form className="d-flex form">
                   <FormControl
                     type="search"
-                    placeholder="Search"
+                    placeholder="Javascript"
                     className="me-2"
                     aria-label="Search"
                   />
-                  <Button type="submit" variant="outline-success">
-                    Search
-                  </Button>
+                  <Button type="submit">Search</Button>
                 </Form>
                 {/* <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Button className="login-btn">Login</Button>
                 </Nav> */}
                 <Nav className="justify-content-end align-items-center">
-                  <div>
-                    <i className="fa-solid fa-bell"></i>
-                    <span className="notification-num">1</span>
-                  </div>
-                  <div style={{ marginLeft: 12 }}>
+                  <Button style={{ marginRight: '10px' }}>Create Post</Button>
+                  <Dropdown>
+                    <Dropdown.Toggle className="position-relative">
+                      <i className="fa-solid fa-bell"></i>
+                      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        99+
+                        <span class="visually-hidden">Notifications</span>
+                      </span>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                      <Dropdown.Item href="#/action-2">
+                        Another action
+                      </Dropdown.Item>
+                      <Dropdown.Item href="#/action-3">
+                        Something else
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                  <div style={{ marginLeft: '25px' }}>
                     <Image
                       src="./lernantino.jpeg"
                       alt="user"
