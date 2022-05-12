4fe4ee40689b707d9c954e8a4eff0b9510714fd6
André Vega <andre_vega@innovativos.mx>
2022-05-11 20:53:37 -0500

    Added project connection with user query information

--- team-finder-app/client/src/pages/Profile.js
+++ team-finder-app/client/src/pages/Profile.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { Container, Card, Nav, Button, Image } from "react-bootstrap";
+import ProfileProjects from "../components/ProfileProjects";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
 import '../components/styles/profile.css';
 import { useParams } from 'react-router-dom';
 import { QUERY_USER } from "../utils/queries";
@@ -7,12 +10,14 @@ import { useQuery } from "@apollo/client";
 import Auth from '../utils/auth';
 import ProfileCard from '../components/ProfileCard';
 import EditProfileForm from '../components/EditProfileForm';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const MyProfile = () => {
 
+  const [displayProjects, setDisplayProjects] = useState();
+  const [profileProjectsData, setProfileProjectsData] = useState();
+
   const { userId } = useParams();
+  // const userId = "6279aa574eea1e4d8c95a783"
 
   const [editMode, setEditMode] = useState(false);
 
@@ -31,40 +36,36 @@ const MyProfile = () => {
       return Auth.getProfile().data._id === user._id;
     }
   };
+  function clickHandler(e) {
+    const id = e.target.id;
+    setDisplayProjects(id);
+    setProfileProjectsData(user);
+    
+  }
+
+  function RenderProjects() {
+    if(displayProjects) {
+      return (
+        <div>
+          <ProfileProjects 
+            displayProjects = {displayProjects}
+            setDisplayProjects = {setDisplayProjects}
+            profileProjectsData = {profileProjectsData}
+            setProfileProjectsData = {setProfileProjectsData}
+          />
+        </div>
+      )
+    } else {
+      return (
+        <div>
+        </div>
+      )
+    }
+  }
 
     return (
       <Container>
-        {data ?
-        <div>
-        <Card className="mb-3 margin-0-auto">
-        <Card.Header>
-          <Nav className="justify-content-between">
-            <Nav.Item>
-              <Nav.Link className="link-primary text-decoration-underline d-none">Edit</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Image src={`../${user.picture}`} alt="user" roundedCircle className='profile-img'></Image>
-            </Nav.Item>
-            <Nav.Item>
-              <a href={"https://github.com/" + user.github} target="_blank"><FontAwesomeIcon icon={faGithub}  className=" fa-icons"/></a> 
-            </Nav.Item>
-          </Nav>
-        </Card.Header>
-        <Card.Body className="text-center">
-          <Card.Title>{user.username} </Card.Title>
-          <Card.Text >
-            Hi, I’m Pamela. I recently graduated from a 6-month full-stack web-development bootcamp and I’m looking to collab on interesting ideas in order to expand my portfolio.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      
-      <h2 className="text-center mb-3 fw-bold">Projects</h2>
-
-      {/* <Nav fill variant="tabs" className="mb-3 fw-bold"  defaultActiveKey="1">
-  return (
-
-    <Container className="main-container">
-      {Auth.loggedIn() && isMe() && !editMode ? (
+        {Auth.loggedIn() && isMe() && !editMode ? (
         <Button onClick={() => setEditMode(true)}>Edit</Button>
       ) : (
         null
@@ -74,40 +75,27 @@ const MyProfile = () => {
       ) : (
         <EditProfileForm user={user} setEditMode={setEditMode} />
       )}
+        {data ?
+        <div>
+      
       <h2 className="text-center mb-3 fw-bold">Projects</h2>
 
-      {/* I don't know how to set a default value without using href as it is seen in documentation */}
-      <Nav fill variant="tabs" className="mb-3 fw-bold" defaultActiveKey="1">
+      {/* <Nav fill variant="tabs" className="mb-3 fw-bold"  defaultActiveKey="1">
+
+    <Container className="main-container"> */}
+      
+
+      <Nav fill variant="tabs" className="mb-3 fw-bold"  defaultActiveKey="1">
         <Nav.Item>
-          <Nav.Link eventKey="link-1">Posted Projects</Nav.Link>
+          <Nav.Link id="posts" onClick={clickHandler} eventKey="link-1">Posted Projects</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2">Joined Projects</Nav.Link>
+          <Nav.Link id="joinedProjects" onClick={clickHandler} eventKey="link-2">Joined Projects</Nav.Link>
         </Nav.Item>
       </Nav>
 
-      <div className="projects-div d-flex flex-column align-items-center">
-        <Card style={{ width: '18rem' }} className="mb-3">
-          <Card.Body className="text-center">
-            <Card.Title>Job Tracker App</Card.Title>
-            <Card.Subtitle className="mt-1 mb-2 fw-bold">HTML, CSS, JavaScript</Card.Subtitle>
-            <Card.Text>
-              This is an app that helps the user keep track of their job applica….
-            </Card.Text>
-            <Button variant="primary">Open</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }} className="mb-3">
-          <Card.Body className="text-center">
-            <Card.Title>Job Tracker App</Card.Title>
-            <Card.Subtitle className="mt-1 mb-2 fw-bold">HTML, CSS, JavaScript</Card.Subtitle>
-            <Card.Text>
-              This is an app that helps the user keep track of their job applica….
-            </Card.Text>
-            <Button variant="danger">Closed</Button>
-          </Card.Body>
-        </Card>
-      </div>  
+      
+      <RenderProjects />
       </div>
       : ""}
       
