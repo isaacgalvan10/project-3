a874e53b251ec66b12e42ccc4b569bd44a982b76
Isaac Galvan <isaacgdev@gmail.com>
2022-05-11 23:19:09 -0500

    updating profile page styles

--- team-finder-app/client/src/components/EditProfileForm.js
+++ team-finder-app/client/src/components/EditProfileForm.js
@@ -1,95 +1,104 @@
-import { Card, Nav, Image, Form, Button, Row, Col } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { Card, Nav, Image, Form, Button, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { useState, useEffect } from 'react';
 import { EDIT_PROFILE } from '../utils/mutations';
-import { useMutation } from "@apollo/client";
+import { useMutation } from '@apollo/client';
 
 const EditProfileForm = ({ user, setEditMode }) => {
-    const [editProfile] = useMutation(EDIT_PROFILE);
+  const [editProfile] = useMutation(EDIT_PROFILE);
 
-    const [editFormData, setEditFormData] = useState({
-        userId: user._id,
-        newUsername: user.username,
-        newBio: user.bio
-    });
+  const [editFormData, setEditFormData] = useState({
+    userId: user._id,
+    newUsername: user.username,
+    newBio: user.bio,
+  });
 
-    const handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        event.preventDefault();
-
-        try {
-            await editProfile({
-                variables: {
-                    ...editFormData
-                }
-            })
-        } catch (e) {
-            console.error(e);
-        }
-
-        setEditFormData({
-            userId: user._id,
-            newUsername: '',
-            newBio: ''
-        });
-
-        setEditMode(false);
+    try {
+      await editProfile({
+        variables: {
+          ...editFormData,
+        },
+      });
+    } catch (e) {
+      console.error(e);
     }
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setEditFormData({ ...editFormData, [name]: value });
-    };
+    setEditFormData({
+      userId: user._id,
+      newUsername: '',
+      newBio: '',
+    });
 
-    return (
-        <Card className="mb-3">
-            <Card.Header>
-                <Nav className="justify-content-between">
-                    <Nav.Item>
-                        <Nav.Link className="link-primary text-decoration-underline d-none">Edit</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Image src={`../${user.picture}`} alt="user" roundedCircle className='profile-img'></Image>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <a href={"https://github.com/" + user.github} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className=" fa-icons" /></a>
-                    </Nav.Item>
-                </Nav>
-            </Card.Header>
-            <Card.Body className="text-center">
-                <Form noValidate onSubmit={handleSubmit}>
-                    <Form.Group className="form-field">
-                        <Form.Label htmlFor='title'>Username</Form.Label>
-                        <Form.Control
-                            type='text'
-                            name='newUsername'
-                            onChange={handleInputChange}
-                            value={editFormData.newUsername}
-                            required
-                        />
-                    </Form.Group>
+    setEditMode(false);
+  };
 
-                    <Form.Group className="form-field">
-                        <Form.Label htmlFor='tags'>Bio</Form.Label>
-                        <Form.Control
-                            as="textarea"
-                            name='newBio'
-                            onChange={handleInputChange}
-                            value={editFormData.newBio}
-                            required
-                        />
-                    </Form.Group>
-                    <Button
-                        type='submit'
-                        variant='success'>
-                        Submit
-                    </Button>
-                    <Button type='submit' variant='success' onClick={() => setEditMode(false)}>Cancel</Button>
-                </Form>
-            </Card.Body>
-        </Card>
-    )
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditFormData({ ...editFormData, [name]: value });
+  };
+
+  return (
+    <Card className="mb-3">
+      <Card.Header>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Image
+              src={`../${user.picture}`}
+              alt="user"
+              roundedCircle
+              className="profile-img"
+            ></Image>
+          </div>
+          <div>
+            <a
+              href={'https://github.com/' + user.github}
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: 'none' }}
+            >
+              <FontAwesomeIcon icon={faGithub} className=" fa-icons" />
+              <p style={{ color: 'black' }}>Github</p>
+            </a>
+          </div>
+        </div>
+      </Card.Header>
+      <Card.Body className="text-center">
+        <Form noValidate onSubmit={handleSubmit}>
+          <Form.Group className="form-field">
+            <Form.Label htmlFor="title">Username</Form.Label>
+            <Form.Control
+              type="text"
+              name="newUsername"
+              onChange={handleInputChange}
+              value={editFormData.newUsername}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group className="form-field">
+            <Form.Label htmlFor="tags">Bio</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="newBio"
+              onChange={handleInputChange}
+              value={editFormData.newBio}
+              required
+            />
+          </Form.Group>
+          <Button type="submit" style={{ marginRight: '10px' }}>
+            Update
+          </Button>
+          <Button type="submit" onClick={() => setEditMode(false)}>
+            Cancel
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default EditProfileForm;
--- team-finder-app/client/src/pages/Profile.js
+++ team-finder-app/client/src/pages/Profile.js
@@ -1,5 +1,16 @@
-import React, { useState } from 'react';
-import { Container, Card, Nav, Button, Image } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Card,
+  Nav,
+  Button,
+  Image,
+  Row,
+  Col,
+  Tabs,
+  Tab,
+  Link,
+} from 'react-bootstrap';
 import ProfileProjects from '../components/ProfileProjects';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -35,7 +46,8 @@ const MyProfile = () => {
       return Auth.getProfile().data._id === user._id;
     }
   };
-  function clickHandler(e) {
+
+  function ClickHandler(e) {
     const id = e.target.id;
     setDisplayProjects(id);
     setProfileProjectsData(user);
@@ -60,49 +72,81 @@ const MyProfile = () => {
 
   return (
     <Container className="main-container">
-      {Auth.loggedIn() && isMe() && !editMode ? (
-        <Button onClick={() => setEditMode(true)}>Edit</Button>
-      ) : null}
-      {!editMode ? (
-        <ProfileCard user={user} />
-      ) : (
-        <EditProfileForm user={user} setEditMode={setEditMode} />
-      )}
-      {data ? (
-        <div>
-          <h2 className="text-center mb-3 fw-bold">Projects</h2>
+      <Row>
+        <Col lg={4}>
+          <h2>Profile</h2>
+          {Auth.loggedIn() && isMe() && !editMode ? (
+            <Button
+              onClick={() => setEditMode(true)}
+              style={{ marginBottom: '10px' }}
+            >
+              Edit profile
+            </Button>
+          ) : null}
+          {!editMode ? (
+            <ProfileCard user={user} />
+          ) : (
+            <EditProfileForm user={user} setEditMode={setEditMode} />
+          )}
+        </Col>
+        <Col lg={8}>
+          {data ? (
+            <div>
+              <h2 className="text-center mb-3">Projects</h2>
 
-          {/* <Nav fill variant="tabs" className="mb-3 fw-bold"  defaultActiveKey="1">
-
-    <Container className="main-container"> */}
-
-          <Nav
-            fill
-            variant="tabs"
-            className="mb-3 fw-bold"
-            defaultActiveKey="1"
-          >
-            <Nav.Item>
-              <Nav.Link id="posts" onClick={clickHandler} eventKey="link-1">
-                Posted Projects
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                id="joinedProjects"
-                onClick={clickHandler}
-                eventKey="link-2"
+              {/* <Nav
+                fill
+                variant="tabs"
+                className="mb-3 fw-bold"
+                defaultActiveKey="1"
               >
-                Joined Projects
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
+                <Nav.Item>
+                  <Nav.Link id="posts" onClick={ClickHandler} eventKey="1">
+                    Posted Projects
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link
+                    id="joinedProjects"
+                    onClick={ClickHandler}
+                    eventKey="link-2"
+                  >
+                    Joined Projects
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
 
-          <RenderProjects />
-        </div>
-      ) : (
-        ''
-      )}
+              <RenderProjects /> */}
+
+              <Tabs
+                defaultActiveKey="my-posts"
+                transition={false}
+                id="noanim-tab-example"
+                className="mb-3"
+              >
+                <Tab eventKey="my-posts" title="My Projects">
+                  <ProfileProjects
+                    displayProjects={'posts'}
+                    setDisplayProjects={'posts'}
+                    profileProjectsData={user}
+                    setProfileProjectsData={user}
+                  />
+                </Tab>
+                <Tab eventKey="joined-rojects" title="Joined Projects">
+                  <ProfileProjects
+                    displayProjects={'joinedProjects'}
+                    setDisplayProjects={'joinedProjects'}
+                    profileProjectsData={user}
+                    setProfileProjectsData={user}
+                  />
+                </Tab>
+              </Tabs>
+            </div>
+          ) : (
+            ''
+          )}
+        </Col>
+      </Row>
     </Container>
   );
 };
