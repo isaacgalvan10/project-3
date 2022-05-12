903c42446b8986eb97ffc0a62e1c600e7d5ede3c
Enoc Rojas <rojiyoyo@gmail.com>
2022-05-11 23:16:32 -0500

    Changed login/signup to a modal

--- team-finder-app/client/src/components/Header.js
+++ team-finder-app/client/src/components/Header.js
@@ -1,5 +1,5 @@
+
 import Container from 'react-bootstrap/Container';
-import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
@@ -15,11 +15,13 @@ import SearchResults from '../pages/SearchResults';
 import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
+import Login from './Login';
+import Signup from './Signup';
 
-import Login from './Login'
-import Signup from './Signup'
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const [state, dispatch] = useGlobalContext();
 
   const [searchValue, setSearchValue] = useState('');
@@ -40,10 +42,6 @@ const Header = () => {
     Auth.logout();
   };
 
-  const [showModal, setShowModal] = useState(false);
-
-  const [state, dispatch] = useGlobalContext();
-
   const displayPostModal = () => {
     dispatch({
       type: SHOW_MODAL,
@@ -58,20 +56,6 @@ const Header = () => {
     const { value } = event.target;
     setSearchValue(value);
   };
-
-  const displayLogin = () => {
-    dispatch({ type: SHOW_MODAL, payload: {
-      request: false,
-      login: true
-  } });
-  }
-
-  const displaySignUp = () => {
-    dispatch({ type: SHOW_MODAL, payload: {
-      request: false,
-      signup: true
-  } });
-  }
 
   return (
     <>
@@ -130,20 +114,12 @@ const Header = () => {
                         </button>
                       ) : (
                         <>
-                          <Link
-                            className="btn"
-                            to="/login"
+                          <Button
                             style={{ marginRight: '10px' }}
+                            onClick={() => setShowModal(true)}
                           >
-                            Login
-                          </Link>
-                          <Link
-                            className="btn"
-                            to="/signup"
-                            style={{ marginRight: '10px' }}
-                          >
-                            Signup
-                          </Link>
+                            Login/Signup
+                          </Button>
                         </>
                       )}
                       {Auth.loggedIn() ? (
@@ -191,6 +167,37 @@ const Header = () => {
           ))}
         </>
       )}
+      <Modal
+            size='lg'
+            show={showModal}
+            onHide={() => setShowModal(false)}
+            aria-labelledby='signup-modal'>
+            {/* tab container to do either signup or login component */}
+            <Tab.Container defaultActiveKey='login'>
+              <Modal.Header closeButton>
+                <Modal.Title id='signup-modal'>
+                  <Nav variant='pills'>
+                    <Nav.Item>
+                      <Nav.Link eventKey='login'>Login</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey='signUp'>Sign Up</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Tab.Content>
+                  <Tab.Pane eventKey='login'>
+                    <Login handleModalClose={() => setShowModal(false)} />
+                  </Tab.Pane>
+                  <Tab.Pane eventKey='signUp'>
+                    <Signup handleModalClose={() => setShowModal(false)} />
+                  </Tab.Pane>
+                </Tab.Content>
+              </Modal.Body>
+            </Tab.Container>
+          </Modal>
     </>
   );
 };
